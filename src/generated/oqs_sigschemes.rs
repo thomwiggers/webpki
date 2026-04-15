426de0c1b06e0016b0ff5b3fa1c32f8bbb310414
Thom Wiggers <thom@thomwiggers.nl>
2026-04-15 14:30:19 +0000

    mldsa

--- src/generated/oqs_sigschemes.rs
+++ src/generated/oqs_sigschemes.rs
@@ -1,11 +1,33 @@
 
-const DILITHIUM2_ID: AlgorithmIdentifier = AlgorithmIdentifier {
-    asn1_id_value: untrusted::Input::from(include_bytes!("../data/alg-dilithium2.der")),
+const MLDSA44_ID: AlgorithmIdentifier = AlgorithmIdentifier {
+    asn1_id_value: untrusted::Input::from(include_bytes!("../data/alg-mldsa44.der")),
 };
 
-/// dilithium2 signatures
-pub static DILITHIUM2: SignatureAlgorithm = SignatureAlgorithm {
-    public_key_alg_id: DILITHIUM2_ID,
-    signature_alg_id: DILITHIUM2_ID,
-    verification_alg: VerificationAlgorithm::Oqs(&oqs::sig::Algorithm::Dilithium2),
+/// mldsa44 signatures
+pub static MLDSA44: SignatureAlgorithm = SignatureAlgorithm {
+    public_key_alg_id: MLDSA44_ID,
+    signature_alg_id: MLDSA44_ID,
+    verification_alg: VerificationAlgorithm::Oqs(&oqs::sig::Algorithm::MlDsa44),
+};
+
+const MLDSA65_ID: AlgorithmIdentifier = AlgorithmIdentifier {
+    asn1_id_value: untrusted::Input::from(include_bytes!("../data/alg-mldsa65.der")),
+};
+
+/// mldsa65 signatures
+pub static MLDSA65: SignatureAlgorithm = SignatureAlgorithm {
+    public_key_alg_id: MLDSA65_ID,
+    signature_alg_id: MLDSA65_ID,
+    verification_alg: VerificationAlgorithm::Oqs(&oqs::sig::Algorithm::MlDsa65),
+};
+
+const MLDSA87_ID: AlgorithmIdentifier = AlgorithmIdentifier {
+    asn1_id_value: untrusted::Input::from(include_bytes!("../data/alg-mldsa87.der")),
+};
+
+/// mldsa87 signatures
+pub static MLDSA87: SignatureAlgorithm = SignatureAlgorithm {
+    public_key_alg_id: MLDSA87_ID,
+    signature_alg_id: MLDSA87_ID,
+    verification_alg: VerificationAlgorithm::Oqs(&oqs::sig::Algorithm::MlDsa87),
 };
--- src/generated/oqs_sigschemes_use.rs
+++ src/generated/oqs_sigschemes_use.rs
@@ -1,4 +1,6 @@
-pub use signed_data::DILITHIUM2;
+pub use signed_data::MLDSA44;
+pub use signed_data::MLDSA65;
+pub use signed_data::MLDSA87;
 pub use signed_data::XMSS1;
 pub use signed_data::XMSS3;
 pub use signed_data::XMSS5;
